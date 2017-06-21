23f27ec6ce36eb6cd52a1cb7c56d58a4f48260df
Yevhenii Zakharov <y.zakharov@astoundcommerce.com>
2017-06-21 13:12:46 +0300

    init commit

---
+++ www/js/routes.js
@@ -0,0 +1,39 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('whoAreYou', {
+    url: '/start',
+    templateUrl: 'templates/whoAreYou.html',
+    controller: 'whoAreYouCtrl'
+  })
+
+  .state('clientControls', {
+    url: '/client',
+    templateUrl: 'templates/clientControls.html',
+    controller: 'clientControlsCtrl'
+  })
+
+  .state('stewardControls', {
+    url: '/stewardControls',
+    templateUrl: 'templates/stewardControls.html',
+    controller: 'stewardControlsCtrl'
+  })
+
+  .state('personalCode', {
+    url: '/generateCode',
+    templateUrl: 'templates/personalCode.html',
+    controller: 'personalCodeCtrl'
+  })
+
+$urlRouterProvider.otherwise('/start')
+
+
+});
